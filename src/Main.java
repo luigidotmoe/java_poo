421a2ecccef58d7bc05b4d6b673b21582bade3bc
Luigi <luigi.joubert@gmail.com>
2024-03-20 10:03:35 +0100

    26

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 import com.cda2.classe.Habitation;
 public class Main {
     public static void main(String[] args) {
-        Habitation habitation1 = new Habitation("Maison", 10, 5);
-        Habitation habitation2 = new Habitation("Appartement", 5, 5);
-        Habitation habitation3 = new Habitation("Villa", 15, 10);
+        Habitation habitation1 = new Habitation("Maison", 10, 5, 2);
+        Habitation habitation2 = new Habitation("Appartement", 5, 5, 1);
+        Habitation habitation3 = new Habitation("Villa", 15, 10, 3);
 
         System.out.println("L'habitation " + habitation1.getNom() + " a une surface de " + habitation1.surface() + " m²");
         System.out.println("L'habitation " + habitation2.getNom() + " a une surface de " + habitation2.surface() + " m²");
--- src/com/cda2/classe/Habitation.java
+++ src/com/cda2/classe/Habitation.java
@@ -6,14 +6,17 @@ public class Habitation {
     private double longueur;
     private double largeur;
 
+    private int nbrEtages;
+
     // constructeurs
     public Habitation() {
     }
 
-    public Habitation(String nom, double longueur, double largeur) {
+    public Habitation(String nom, double longueur, double largeur, int nbrEtages) {
         this.nom = nom;
         this.longueur = longueur;
         this.largeur = largeur;
+        this.nbrEtages = nbrEtages;
     }
 
     //getter et setters
@@ -41,8 +44,16 @@ public class Habitation {
         this.largeur = largeur;
     }
 
+    public int getNbrEtages() {
+        return nbrEtages;
+    }
+
+    public void setNbrEtages(int nbrEtages) {
+        this.nbrEtages = nbrEtages;
+    }
+
     //méthode
     public double surface() {
-        return this.longueur * this.largeur;
+        return this.longueur * this.largeur * this.nbrEtages;
     }
 }
